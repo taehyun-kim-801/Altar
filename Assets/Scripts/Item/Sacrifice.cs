4182214a43285232d37978212d48c52695d7d3d9
SCOUTgit <k01095232802@gmail.com>
2019-08-08 01:02:09 +0900

    [Add] 음식(close #41)

---
+++ Assets/Scripts/Item/Sacrifice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sacrifice : Item
+{
+    public readonly int satiety;
+    public readonly int damage;
+
+    public Sacrifice(string name, Sprite sprite, int satiety, int hurt) : base(name, sprite)
+    {
+        this.satiety = satiety;
+        this.damage = hurt;
+    }
+
+    public override void UseItem(GameObject gameObject)
+    {
+        gameObject.SendMessage("Eat", satiety);
+        gameObject.SendMessage("Hurt", damage);
+    }
+}
