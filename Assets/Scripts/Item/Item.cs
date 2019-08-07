763c88c62d92b1a765d9d2ed4d008cb805bf6f6f
SCOUTgit <k01095232802@gmail.com>
2019-08-08 01:01:27 +0900

    [Add] 아이템(close #25)

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item
+{
+    public readonly string name;
+    public readonly Sprite sprite;
+
+    protected Item(string name, Sprite sprite)
+    {
+        this.name = name;
+        this.sprite = sprite;
+    }
+
+    public abstract void UseItem(GameObject gameObject);
+}
