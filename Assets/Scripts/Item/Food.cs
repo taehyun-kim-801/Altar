418c2a89645c6e3b465612cc74c4868e63a6dc35
SCOUTgit <k01095232802@gmail.com>
2019-08-08 01:01:49 +0900

    [Add] 음식(close #42)

---
+++ Assets/Scripts/Item/Food.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : Item
+{
+    public readonly int satiety;
+
+    public Food(string name, Sprite sprite, int satiety) : base(name, sprite)
+    {
+        this.satiety = satiety;
+    }
+
+    public override void UseItem(GameObject gameObject)
+    {
+        gameObject.SendMessage("Eat", satiety);
+    }
+}
