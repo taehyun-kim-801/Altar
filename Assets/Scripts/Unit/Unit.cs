feccf7800907bafa14fae3e5568b02c1443d8863
김태현 <allone1129@gmail.com>
2019-08-08 22:38:56 +0900

    Add Unit

---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    [SerializeField]
+    private float _health;
+
+    
+    public float health
+    {
+        get { return _health; }
+        set {
+            _health = Mathf.Clamp(value, 0, 100);
+        }
+    }
+
+    protected Vector3 faceDirection;
+
+    public Vector3 FaceDirection
+    {
+        get { return faceDirection; }
+    }
+
+    protected void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public float moveSpeed;
+
+    public void Move(Vector3 direction)
+    {
+        gameObject.transform.Translate(moveSpeed * direction);
+    }
+
+    
+
+    public abstract void Interaction();
+
+    public abstract void GetAttack(GameObject enemy);
+}
