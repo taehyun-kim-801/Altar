daec657c01503abe7f02aba64699f81623872b0c
김태현 <allone1129@gmail.com>
2019-08-12 15:50:21 +0900

    Add camera script

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        if (player != null)
+            this.transform.position = player.position + new Vector3(0, 0, -10);
+    }
+}
