1660d73d1a9def51887ee11b2b6ca474fbfb8c71
김태현 <allone1129@gmail.com>
2019-09-08 14:49:24 +0900

    [Change] MapManager 기능 분할

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -3,76 +3,25 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.U2D;
+using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour
 {
-    public static MapManager Instance { get; private set; }
+    public Transform player;
 
-    private Dictionary<string, List<string>> monstersByMap;
-    private Dictionary<string, MonsterInfo> monsters;
+    public Tilemap tilemap;
 
-    private Transform player;
+    private Vector2 minTilemap;
+    private Vector2 maxTilemap;
 
-    private SpriteAtlas atlas;
-    private void Awake()
-    {
-        if(Instance == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            Instance = this;
-        }
-        else
-        {
-            Destroy(this);
-            return;
-        }
-
-        monstersByMap = new Dictionary<string, List<string>>();
-        monsters = new Dictionary<string, MonsterInfo>();
-
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        if(!File.Exists($"{Application.dataPath}/Data/MonsterInfo.json"))
-        {
-            FileStream fs = File.Create($"{Application.dataPath}/Data/MonsterInfo.json");
-            fs.Close();
-            JsonManager.SaveJson(SaveMonsters());
-        }
-
-
-        if(!File.Exists($"{Application.dataPath}/Data/MonsterList.json"))
-        {
-            FileStream fs = File.Create($"{Application.dataPath}/Data/MonsterList.json");
-            fs.Close();
-            JsonManager.SaveJson(SaveLists());
-        }
-
-        atlas = Resources.Load<SpriteAtlas>("Units");
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        LoadMonstersJson();
+        minTilemap = tilemap.CellToWorld(tilemap.cellBounds.min);
+        maxTilemap = tilemap.CellToWorld(tilemap.cellBounds.max);
+
+        Debug.Log(minTilemap);
+        Debug.Log(maxTilemap);
         StartCoroutine(Spawn());
-    }
-
-    private void LoadMonstersJson()
-    {
-        JsonManager.LoadJson<MonsterInfo>().ForEach((monster) => { monsters.Add(monster.Name, monster); });
-
-        foreach(var monster in monsters)
-        {
-            Debug.Log(monster.Key + " " + monster.Value);
-        }
-
-        JsonManager.LoadJson<MonsterList>().ForEach((list) => { monstersByMap.Add(list.name, list.monsters); });
-
-        foreach (var list in monstersByMap)
-        {
-            Debug.Log(list.Key + " " + list.Value);
-        }
     }
 
     private IEnumerator Spawn()
@@ -83,9 +32,9 @@ public class MapManager : MonoBehaviour
 
             float randX = Random.Range(-10.0f, 10.0f);
             float randY = Random.Range(-10.0f, 10.0f);
-            int spawnIdx = Random.Range(1, monstersByMap[SceneManager.GetActiveScene().name].Count + 1);
+            int spawnIdx = Random.Range(1, MonsterManager.monstersByMap[SceneManager.GetActiveScene().name].Count + 1);
 
-            GameObject spawnObj = new GameObject(monstersByMap[SceneManager.GetActiveScene().name][spawnIdx - 1]);
+            GameObject spawnObj = new GameObject(MonsterManager.monstersByMap[SceneManager.GetActiveScene().name][spawnIdx - 1]);
             spawnObj.transform.localScale = new Vector2(5f, 5f);
             var monster = spawnObj.AddComponent<Monster>();
             spawnObj.tag = "Monster";
@@ -96,52 +45,19 @@ public class MapManager : MonoBehaviour
             monsterRB.gravityScale = 0f;
             SpriteRenderer render = spawnObj.AddComponent<SpriteRenderer>();
 
-            render.sprite = atlas.GetSprite($"{monstersByMap[SceneManager.GetActiveScene().name][spawnIdx - 1]}_1");
+            render.sprite = DataContainer.monsterAtlas.GetSprite($"{MonsterManager.monstersByMap[SceneManager.GetActiveScene().name][spawnIdx - 1]}_1");
 
-            MonsterInfo spawnInfo = monsters[monstersByMap[SceneManager.GetActiveScene().name][spawnIdx - 1]];
+            MonsterInfo spawnInfo = MonsterManager.monsters[MonsterManager.monstersByMap[SceneManager.GetActiveScene().name][spawnIdx - 1]];
             monster.Health = spawnInfo.Health;
             monster.name = spawnInfo.Name;
             monster.damage = spawnInfo.Damage;
             monster.dropItem = spawnInfo.DropItem;
 
             monster.attackWaitSecond = 5f;
-            
-            Instantiate(monster, new Vector3(player.position.x + randX, player.position.y + randY), new Quaternion(0, 0, 0, 0));
+
+            Instantiate(monster, new Vector3(Mathf.Clamp(player.position.x + randX, minTilemap.x, maxTilemap.x),
+                Mathf.Clamp(player.position.y + randY, minTilemap.y, maxTilemap.y)), new Quaternion(0, 0, 0, 0));
             Destroy(spawnObj);
         }
-    }
-
-    [System.Serializable]
-    private struct MonsterList
-    {
-        [SerializeField]
-        public string name;
-        [SerializeField]
-        public List<string> monsters;
-
-        public MonsterList(string name,List<string> monsters)
-        {
-            this.name = name;
-            this.monsters = monsters;
-        }
-    }
-
-    private List<MonsterInfo> SaveMonsters() 
-    {
-        List<MonsterInfo> result = new List<MonsterInfo>();
-
-        result.Add(new MonsterInfo("Skeleton", 10, 1, "Bone"));
-        result.Add(new MonsterInfo("Slime", 20, 2, "Jelly"));
-        result.Add(new MonsterInfo("StoneGolem", 30, 3, "Stone"));
-
-        return result;
-    }
-
-    private List<MonsterList> SaveLists()
-    {
-        List<MonsterList> result = new List<MonsterList>();
-
-        result.Add(new MonsterList("Grave",new List<string> { "Skeleton","Slime","StoneGolem" }));
-        return result;
     }
 }
---
+++ Assets/Scripts/Unit/Monster/MonsterManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class MonsterManager
+{
+    public static Dictionary<string, List<string>> monstersByMap { get; private set; }
+    public static Dictionary<string, MonsterInfo> monsters { get; private set; }
+
+    static MonsterManager()
+    {
+        monstersByMap = new Dictionary<string, List<string>>();
+        monsters = new Dictionary<string, MonsterInfo>();
+        
+        if(!File.Exists($"{Application.dataPath}/Data/MonsterInfo.json"))
+        {
+            FileStream fs = File.Create($"{Application.dataPath}/Data/MonsterInfo.json");
+            fs.Close();
+            JsonManager.SaveJson(SaveMonsters());
+        }
+
+        if (!File.Exists($"{Application.dataPath}/Data/MonsterList.json"))
+        {
+            FileStream fs = File.Create($"{Application.dataPath}/Data/MonsterList.json");
+            fs.Close();
+            JsonManager.SaveJson(SaveLists());
+        }
+
+        LoadMonstersJson();
+    }
+
+    private static void LoadMonstersJson()
+    {
+        JsonManager.LoadJson<MonsterInfo>().ForEach((monster) => { monsters.Add(monster.Name, monster); });
+
+        foreach (var monster in monsters)
+        {
+            Debug.Log(monster.Key + " " + monster.Value);
+        }
+
+        JsonManager.LoadJson<MonsterList>().ForEach((list) => { monstersByMap.Add(list.name, list.monsters); });
+
+        foreach (var list in monstersByMap)
+        {
+            Debug.Log(list.Key + " " + list.Value);
+        }
+    }
+
+    [System.Serializable]
+    private struct MonsterList
+    {
+        [SerializeField]
+        public string name;
+        [SerializeField]
+        public List<string> monsters;
+
+        public MonsterList(string name, List<string> monsters)
+        {
+            this.name = name;
+            this.monsters = monsters;
+        }
+    }
+
+    private static List<MonsterInfo> SaveMonsters()
+    {
+        List<MonsterInfo> result = new List<MonsterInfo>();
+
+        result.Add(new MonsterInfo("Skeleton", 10, 1, "Bone"));
+        result.Add(new MonsterInfo("MiniOrk", 10, 1, "Mask"));
+        result.Add(new MonsterInfo("RipDevil", 10, 1, "Devil's Rip"));
+        result.Add(new MonsterInfo("MagicianOrk", 20, 2, "Staff"));
+        result.Add(new MonsterInfo("Ghost", 20, 2, "Hoot"));
+        result.Add(new MonsterInfo("Slime", 20, 2, "Jelly"));
+        result.Add(new MonsterInfo("StoneGolem", 30, 3, "Stone"));
+        result.Add(new MonsterInfo("Satan", 30, 3, "Tooth"));
+        result.Add(new MonsterInfo("CaptainOrk", 30, 3, "Leather"));
+
+        return result;
+    }
+
+    private static List<MonsterList> SaveLists()
+    {
+        List<MonsterList> result = new List<MonsterList>();
+
+        result.Add(new MonsterList("Grave", new List<string> { "Skeleton", "Slime", "StoneGolem" }));
+        result.Add(new MonsterList("Hell", new List<string> { "RipDevil", "Ghost", "Satan" }));
+        result.Add(new MonsterList("Forest", new List<string> { "MiniOrk", "MagicianOrk", "CaptainOrk" }));
+        return result;
+    }
+}
